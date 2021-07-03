acf385bad9a9eae874a7860085199e8833287e40
Alex Oxorn <alex.oxorn@gmail.com>
2021-07-03 19:56:11 -0400

    clear up :p

--- games/sonic_adventure_2_gc/variables.hpp
+++ games/sonic_adventure_2_gc/variables.hpp
@@ -4,11 +4,6 @@
 
 // OFFSET 7ea811a8
 
-// constexpr u32 normal_stage_ranks = 0x8028A2C0;
-// constexpr u32 extra_stage_ranks = 0x8028A4DC;
-// constexpr u32 extra_times_ranks = 0x8028A74C;
-// constexpr u32 boss_times_ranks = 0x8028A608;
-
 // STEAM: 0x174B050
 constexpr u32 score_address1 = 0x801B3D0C; // 7ea68cbc
 constexpr u32 score_address2 = 0x801CC1F8; // 7ea811a8
@@ -25,17 +20,6 @@ constexpr u32 stage_data_next = 0x801E7B68;
 
 
 constexpr u32 stage_data = 0x801F25C4;
-
-// constexpr u32 high_score_address = 0x803E7798;
-// constexpr u32 extra_flag_address = 0x80452C48;
-
-// constexpr u32 game_state = 0x8029C31C;
-
-
-
-
-
-
 
 // 3550
 //-1600
