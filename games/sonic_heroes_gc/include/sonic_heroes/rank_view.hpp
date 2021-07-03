53941c1f01c52077222811b11350fb17df427b24
Alex Oxorn <alex.oxorn@gmail.com>
2021-07-03 02:24:15 -0400

    extract rank_view

---
+++ games/sonic_heroes_gc/include/sonic_heroes/rank_view.hpp
@@ -0,0 +1,24 @@
+#include <dolphin_process.hpp>
+#include <rank.hpp>
+#include <ox/formating.h>
+#include <sonic_heroes/data_extractor.hpp>
+#include <fmt/core.h>
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+
+constexpr int stall_time_milli = 2500;
+
+struct state {
+	int level;
+	int team;
+	int mission;
+
+	bool operator==(state other) const {
+		return level == other.level &&
+				team == other.team &&
+				mission == other.mission;
+	}
+};
+
+void display_ranks(int pid);
--- main.cpp
+++ main.cpp
@@ -1,33 +1,4 @@
-#include "memory/dolphin_process.hpp"
-#include <rank.hpp>
-#include <ox/formating.h>
-#include <sonic_heroes/data_extractor.hpp>
-#include <fmt/core.h>
-#include <iostream>
-#include <cstdlib>
-#include <unistd.h>
-
-constexpr int stall_time_milli = 2500;
-
-struct LevelRankScores{
-	int16_t Level;
-	int16_t DRank;
-	int16_t CRank;
-	int16_t BRank;
-	int16_t ARank;
-};
-
-struct state {
-	int level;
-	int team;
-	int mission;
-
-	bool operator==(state other) const {
-		return level == other.level &&
-				team == other.team &&
-				mission == other.mission;
-	}
-};
+#include <sonic_heroes/rank_view.hpp>
 
 int main(int argc, char** argv) {
 	int pid = -1;
@@ -38,52 +9,5 @@ int main(int argc, char** argv) {
 
 	pid = atoi(argv[1]);
 
-    dolphin_process dolphin{pid};
-	normal_stages prototype{};
-	void* buffer = &prototype;
-
-	std::array<rank_data, 4> s_ranks{};
-	std::array<time_rank_data, 4> t_ranks{};
-
-
-	state current{-1, -1, -1};
-	int result = 0;
-
-	while(true) {
-		usleep(stall_time_milli);
-		state next{get_level(dolphin), get_team(dolphin), get_mission(dolphin)};
-        std::cout << std::flush;
-
-		if(next != current) {
-            fmt::print("{}{}", ox::format{ox::escape::reset}, ox::clear_screen{ox::escape::all});
-			current = next;
-			result = get_ranks(dolphin, current.level, current.team, current.mission, buffer);
-			if((result & TIMED_LEVEL) != 0) {
-				if((result & BOSS_LEVEL) != 0) {
-					t_ranks = interpret_time_rank_data(dolphin, reinterpret_cast<boss_timed_stages *>(buffer), current.team);
-				} else if ((result & EXTRA_MISSION) != 0) {
-					t_ranks = interpret_time_rank_data(dolphin, reinterpret_cast<extra_timed_stages *>(buffer), current.team);
-				} else {
-					return -1;
-				}
-			} else {
-				if((result & EXTRA_MISSION) != 0) {
-					s_ranks = interpret_score_rank_data(dolphin, reinterpret_cast<extra_stages *>(buffer), current.team);
-				} else {
-					s_ranks = interpret_score_rank_data(dolphin, reinterpret_cast<normal_stages *>(buffer), current.team);
-				}
-			}
-		}
-
-		if (result == -1)
-			continue;
-
-		if ((result & TIMED_LEVEL) != 0) {
-			std::array<u8, 3> time = get_time(dolphin);
-			print_current_progress(t_ranks, {time[0] * 60 + time[1], ox::format{ox::escape::background_white, ox::escape::white}});
-		} else {
-			interpret_score(dolphin);
-			print_current_progress(s_ranks, interpret_score(dolphin), 50000);
-		}
-	}
+    display_ranks(pid);
 }
